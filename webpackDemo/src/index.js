5e95fb419245849743258952b87d8c5bafd153ed
xuech <xuech_love@126.com>
2020-12-22 15:04:21 +0800

    eslint

--- webpackDemo/src/index.js
+++ webpackDemo/src/index.js
@@ -1,11 +1,11 @@
-import "./assets/index.css";
-import "./assets/user.less";
+import './assets/index.css';
+import './assets/user.less';
 // weboack能处理js/json资源，不能处理css/img资源
-function add(x,y) {
-  return x+y
+function add(x, y) {
+  return x + y;
 }
 // [a,b,c] = [3,2,1]
-const a = 'hello'
-const str = `${a} world`
+const a = 'hello';
+const str = `${a} world`;
 console.log(str);
-console.log(add(1,2));
+console.log(add(1, 2));
