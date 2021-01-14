e025f8773c59e18f409c99385fca0a815a5264f0
xuech <xuechunhua@med-log.cn>
2021-01-14 17:18:33 +0800

    xx

--- css/index.js
+++ css/index.js
@@ -31,30 +31,30 @@
 
 // step();
 
-function createAssigner (keysFunc, defaults) {
-    return function (obj) {
-      var length = arguments.length;
-      if (defaults) obj = Object(obj);
-      if (length < 2 || obj == null) return obj;
-      for (var index = 1; index < length; index++) {
-        var source = arguments[index],
-            keys = keysFunc(source),
-            l = keys.length;
-        for (var i = 0; i < l; i++) {
-          var key = keys[i];
-          if (!defaults || obj[key] === void 0) obj[key] = source[key];
-        }
-      }
-      return obj;
-    };
-  }
-function allKeys(obj) {
-    var keys = [];
-    for (var key in obj) keys.push(key);
-    console.log(keys);
-    return keys;
-}
+// function createAssigner (keysFunc, defaults) {
+//     return function (obj) {
+//       var length = arguments.length;
+//       if (defaults) obj = Object(obj);
+//       if (length < 2 || obj == null) return obj;
+//       for (var index = 1; index < length; index++) {
+//         var source = arguments[index],
+//             keys = keysFunc(source),
+//             l = keys.length;
+//         for (var i = 0; i < l; i++) {
+//           var key = keys[i];
+//           if (!defaults || obj[key] === void 0) obj[key] = source[key];
+//         }
+//       }
+//       return obj;
+//     };
+//   }
+// function allKeys(obj) {
+//     var keys = [];
+//     for (var key in obj) keys.push(key);
+//     console.log(keys);
+//     return keys;
+// }
 
-var extend = createAssigner(allKeys, true);
-var aaa = extend({ "a": 1, "b": 2 },{ "a": 3, "c": 4 })
+// var extend = createAssigner(allKeys, true);
+// var aaa = extend({ "a": 1, "b": 2 },{ "a": 3, "c": 4 })
 // console.log(aaa);
