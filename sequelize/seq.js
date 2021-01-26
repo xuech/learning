baec0dffa08f656760f9a2c5e0a76454f1cd90e7
xuech <xuechunhua@med-log.cn>
2021-01-26 10:25:41 +0800

    pool

--- sequelize/seq.js
+++ sequelize/seq.js
@@ -2,7 +2,12 @@ const Sequelize = require("sequelize")
 
 const seq = new Sequelize('koa2_weibo_db', 'root', '123456', {
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
 })
 module.exports = seq
 
